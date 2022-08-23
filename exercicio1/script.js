f36aaa008b52b73f11bb7a418f590f6c6a34d68d
Luiz Dirceu <juninholdmarques@gmail.com>
2022-08-23 19:57:49 -0300

    atividade

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+// function imprimirOla (nome){
+//     console.log(`ola ${nome}`)
+// } 
+// imprimirOla("Luiz")
+
+
+function tabuada(array){
+    for (let i of array){
+        console.log(i*6)
+    }
+}
+const final = tabuada([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
